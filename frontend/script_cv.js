7d23a061fa4ef2ffcb9f1f9570a8c21904b37700
Idhibhat Pankam <marginpankam@gmail.com>
2023-04-26 13:57:45 +0700

    feat(frontend): prettier and getCourseMaterialsFromDb

--- frontend/script_cv.js
+++ frontend/script_cv.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = '127.0.0.1:3000';
 
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
@@ -13,8 +13,8 @@ const getGroupNumber = () => {
 // Example: Send Get user profile ("GET") request to backend server and show the response on the webpage
 const getUserProfile = async () => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
   await fetch(
     `http://${backendIPAddress}/courseville/get_profile_info`,
@@ -24,10 +24,10 @@ const getUserProfile = async () => {
     .then((data) => {
       console.log(data.user);
       document.getElementById(
-        "eng-name-info"
+        'eng-name-info'
       ).innerHTML = `${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
       document.getElementById(
-        "thai-name-info"
+        'thai-name-info'
       ).innerHTML = `${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
     })
     .catch((error) => console.error(error));
@@ -37,8 +37,8 @@ const getUserProfile = async () => {
 //            and display the result on the webpage
 const getCompEngEssCid = async () => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -49,16 +49,16 @@ const getCompEngEssCid = async () => {
     .catch((error) => console.error(error));
 
   const cv_cid = data.data.student.find(
-    (std) => std.course_no === "2110221"
+    (std) => std.course_no === '2110221'
   ).cv_cid;
 
-  document.getElementById("ces-cid-value").innerHTML = cv_cid;
+  document.getElementById('ces-cid-value').innerHTML = cv_cid;
 };
 
 const getCourses = async () => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -68,24 +68,17 @@ const getCourses = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    
-    
-    const cv_cid_array = data.data.student.map(
-      (c) => c.cv_cid
-    );
-  
-    
-    cv_cid_array.forEach(async (cv_cid) => {
-      await getCourseInfo(cv_cid);
-    });
-   
+  const cv_cid_array = data.data.student.map((c) => c.cv_cid);
 
-}
+  cv_cid_array.forEach(async (cv_cid) => {
+    await getCourseInfo(cv_cid);
+  });
+};
 
 const getCourseInfo = async (cv_cid) => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -95,13 +88,12 @@ const getCourseInfo = async (cv_cid) => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    const select = document.getElementById("subject-select");
+  const select = document.getElementById('subject-select');
 
-    select.innerHTML += `<option value="${cv_cid}">${data.data.title}</option>`;
+  select.innerHTML += `<option value="${cv_cid}">${data.data.title}</option>`;
 
   console.log(data);
 };
-
 
 /*const getCourseInfo = async () => {
   //const cv_cid = document.getElementById("courseIdInput0").value;
@@ -122,11 +114,11 @@ const getCourseInfo = async (cv_cid) => {
 }*/
 
 const getCourseMaterials = async () => {
-  const cv_cid = document.getElementById("subject-select").value;
+  const cv_cid = document.getElementById('subject-select').value;
   console.log(cv_cid);
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -136,14 +128,32 @@ const getCourseMaterials = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    console.log(data);
-}
+  console.log(data);
+};
+
+const getCourseMaterialsFromDb = async () => {
+  const cv_cid = document.getElementById('subject-select').value;
+  console.log(cv_cid);
+  const options = {
+    method: 'GET',
+    credentials: 'include',
+  };
+
+  const data = await fetch(
+    `http://${backendIPAddress}/items/${cv_cid}`,
+    options
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+
+  console.log(data);
+};
 
 const getMaterial = async () => {
-  const item_id = document.getElementById("itemIdInput").value;
+  const item_id = document.getElementById('itemIdInput').value;
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -153,14 +163,14 @@ const getMaterial = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    console.log(data);
-}
+  console.log(data);
+};
 
 const getCourseMaterialsLinks = async () => {
-  const cv_cid = document.getElementById("courseIdInput2").value;
+  const cv_cid = document.getElementById('courseIdInput2').value;
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -170,14 +180,14 @@ const getCourseMaterialsLinks = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    console.log(data);
-}
+  console.log(data);
+};
 
 const addAllAvailableItems = async () => {
-  const cv_cid = document.getElementById("courseIdInput3").value;
+  const cv_cid = document.getElementById('courseIdInput3').value;
   const options = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -187,64 +197,64 @@ const addAllAvailableItems = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-    console.log(data);
-}
+  console.log(data);
+};
 
 const downloadSelectedItems = async () => {
-  const cv_cid = document.getElementById("courseIdInput3").value;
+  const cv_cid = document.getElementById('courseIdInput3').value;
   const options = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify([{id:837512},{id:837754},{id:830878}]),
+    body: JSON.stringify([{ id: 837512 }, { id: 837754 }, { id: 830878 }]),
   };
 
   const data = await fetch(
     `http://${backendIPAddress}/items/download_selected`,
     options
   )
-  .then( res => res.blob() )
-  .then( blob => {
-    const file = window.URL.createObjectURL(blob);
-    window.location.assign(file);
-  })
-  .catch((error) => console.error(error));
-    // .then((response) => response.json())
+    .then((res) => res.blob())
+    .then((blob) => {
+      const file = window.URL.createObjectURL(blob);
+      window.location.assign(file);
+    })
+    .catch((error) => console.error(error));
+  // .then((response) => response.json())
 
-    // console.log(data);
-    // const fetchOptions = {
-    //   headers:{
-    //     "Access-Control-Allow-Origin": "*",
-    //   }
-    // };
-    // for(let i=0;i<data.length;i++){
-    //   await fetch(data[i], fetchOptions).then( res => res.blob() )
-    //   .then( blob => {
-    //     var url = window.URL.createObjectURL(blob);
-    //     var a = document.createElement('a');
-    //     a.href = url;
-    //     a.setAttribute("download", "");
-    //     // a.target = "_blank"
-    //     // a.download = "filename.xlsx";
-    //     document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-    //     a.click();    
-    //     a.remove(); 
-    //   });
-    // }
-}
+  // console.log(data);
+  // const fetchOptions = {
+  //   headers:{
+  //     "Access-Control-Allow-Origin": "*",
+  //   }
+  // };
+  // for(let i=0;i<data.length;i++){
+  //   await fetch(data[i], fetchOptions).then( res => res.blob() )
+  //   .then( blob => {
+  //     var url = window.URL.createObjectURL(blob);
+  //     var a = document.createElement('a');
+  //     a.href = url;
+  //     a.setAttribute("download", "");
+  //     // a.target = "_blank"
+  //     // a.download = "filename.xlsx";
+  //     document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+  //     a.click();
+  //     a.remove();
+  //   });
+  // }
+};
 
 // TODO #3.5: Send Get Course Assignments ("GET") request with cv_cid to backend server
 //            and create Comp Eng Ess assignments table based on the response (itemid, title)
 const createCompEngEssAssignmentTable = async () => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
-  const cv_cid = document.getElementById("ces-cid-value").innerHTML;
+  const table_body = document.getElementById('main-table-body');
+  table_body.innerHTML = '';
+  const cv_cid = document.getElementById('ces-cid-value').innerHTML;
 
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
 
   const data = await fetch(
@@ -254,7 +264,7 @@ const createCompEngEssAssignmentTable = async () => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-  const table = document.getElementById("main-table-body");
+  const table = document.getElementById('main-table-body');
 
   data.data.map((ass) => {
     table.innerHTML += `<tr> 
@@ -273,4 +283,4 @@ const logout = async () => {
   window.location.href = `http://${backendIPAddress}/courseville/logout`;
 };
 
-document.getElementById("group-id").innerHTML = getGroupNumber();
+document.getElementById('group-id').innerHTML = getGroupNumber();
--- frontend/script_item.js
+++ frontend/script_item.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "10.201.236.173:3000";
+const backendIPAddress = '10.201.236.173:3000';
 
 let itemsData;
 
@@ -10,14 +10,14 @@ const getGroupNumber = () => {
 
 // TODO #2.2: Show group members
 const showGroupMembers = async () => {
-  const member_list = document.getElementById("member-list");
-  member_list.innerHTML = "";
-  const member_dropdown = document.getElementById("name-to-add");
+  const member_list = document.getElementById('member-list');
+  member_list.innerHTML = '';
+  const member_dropdown = document.getElementById('name-to-add');
   member_dropdown.innerHTML =
     "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
   await fetch(`http://${backendIPAddress}/items/members`, options)
     .then((response) => response.json())
@@ -41,8 +41,8 @@ const showGroupMembers = async () => {
 // TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
 const getItemsFromDB = async () => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
   itemsData = await fetch(`http://${backendIPAddress}/items`, options)
     .then((r) => r.json())
@@ -51,8 +51,8 @@ const getItemsFromDB = async () => {
 
 // TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
 const showItemsInTable = (itemsData) => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
+  const table_body = document.getElementById('main-table-body');
+  table_body.innerHTML = '';
   // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
   itemsData.sort((a, b) => a.created_date - b.created_date);
   // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
@@ -72,15 +72,15 @@ const showItemsInTable = (itemsData) => {
 
 // TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
 const addItem = async () => {
-  const item = document.getElementById("item-to-add").value;
-  const name = document.getElementById("name-to-add").value;
-  const price = document.getElementById("price-to-add").value;
+  const item = document.getElementById('item-to-add').value;
+  const name = document.getElementById('name-to-add').value;
+  const price = document.getElementById('price-to-add').value;
 
   await fetch(`http://${backendIPAddress}/items`, {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ item, name, price }),
   });
@@ -91,8 +91,8 @@ const addItem = async () => {
 // TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
 const deleteItem = async (item_id) => {
   await fetch(`http://${backendIPAddress}/items/${item_id}`, {
-    method: "DELETE",
-    credentials: "include",
+    method: 'DELETE',
+    credentials: 'include',
   });
 
   redrawDOM();
@@ -100,22 +100,22 @@ const deleteItem = async (item_id) => {
 
 const redrawDOM = () => {
   window.document.dispatchEvent(
-    new Event("DOMContentLoaded", {
+    new Event('DOMContentLoaded', {
       bubbles: true,
       cancelable: true,
     })
   );
-  document.getElementById("item-to-add").value = "";
-  document.getElementById("name-to-add").value = "0";
-  document.getElementById("price-to-add").value = "";
+  document.getElementById('item-to-add').value = '';
+  document.getElementById('name-to-add').value = '0';
+  document.getElementById('price-to-add').value = '';
 };
 
-document.getElementById("group-no").innerHTML = getGroupNumber();
+document.getElementById('group-no').innerHTML = getGroupNumber();
 
-document.addEventListener("DOMContentLoaded", async function (event) {
-  console.log("Showing group members.");
+document.addEventListener('DOMContentLoaded', async function (event) {
+  console.log('Showing group members.');
   await showGroupMembers();
-  console.log("Showing items from database.");
+  console.log('Showing items from database.');
   await getItemsFromDB();
   showItemsInTable(itemsData);
 });
