2764b1e668521a387902fe937796b2d30d48be38
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-04-30 23:21:07 +0700

    Frontend(Creat select all)

--- frontend/script_table.js
+++ frontend/script_table.js
@@ -35,7 +35,35 @@ export const createMaterialsTable = async () => {
   });
   document.getElementById("command1").style.display = "";
   document.getElementById("add-block").style.display = "";
+  CheckAll();
+
+   
 
 
 };
 window.createMaterialsTable = createMaterialsTable;
+
+function CheckAll(){
+  // Get all the checkboxes in the table
+  var checkboxes = document.querySelectorAll("table input[type='checkbox']");
+
+  // Get the "select all" checkbox
+  var selectAllCheckbox = document.getElementById("select-all-checkbox");
+
+  // Add a change event listener to the "select all" checkbox
+  selectAllCheckbox.addEventListener("change", function() {
+    // Toggle each checkbox in the table based on the "select all" checkbox state
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = selectAllCheckbox.checked;
+    });
+  });
+
+  // Add a change event listener to each checkbox in the table
+ checkboxes.forEach(function(checkbox) {
+ checkbox.addEventListener("change", function() {
+   // Set the "select all" checkbox state based on the state of the other checkboxes
+   selectAllCheckbox.checked = checkboxes.length === document.querySelectorAll("table input[type='checkbox']:checked").length;
+ });
+});
+
+}
